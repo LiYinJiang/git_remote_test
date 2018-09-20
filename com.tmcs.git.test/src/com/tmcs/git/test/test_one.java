c7abe0725955a2d4257464af9ec86aa9bf577913
liYingJiang <lyj222555@gmail.com>
2018-09-20 09:45:59 +0800

    create_temp_branch

--- com.tmcs.git.test/src/com/tmcs/git/test/test_one.java
+++ com.tmcs.git.test/src/com/tmcs/git/test/test_one.java
@@ -2,5 +2,10 @@ package com.tmcs.git.test;
 
 // 2018Äê9ÔÂ20ÈÕ09:35:48
 public class test_one {
-
+	public static void main(String[] args) {
+		
+		//branch temp
+		System.out.println("branch temp");
+		
+	}
 }
